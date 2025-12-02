b23857a7c6a339c194b8cb7c2e53d96ccb93b085
Nezusa <123406875+Nezzusa@users.noreply.github.com>
2025-12-02 12:29:13 +0100

    com2

--- finalProject/main.py
+++ finalProject/main.py
@@ -1,25 +1,85 @@
-
-def errorCalc(Tw, current_temp, tp, ti):
-    error_sum = 0
-    new_error = Tw - current_temp #Tw - t[n]
-    error_sum += new_error
-    e = (tp / ti) * error_sum
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output
+import plotly.graph_objs as go
 
 
-def heat_calculation(tp,ti):
-    t = []
+def errorCalc(requested_water_temp, current_temp, sampling_period, integration_constant):
+    new_error = requested_water_temp - current_temp
+    I_term = (sampling_period / integration_constant) * new_error
+    return new_error, I_term
+
+
+def calculateTankArea(tank_height, tank_bottom_area):
+    return tank_height * tank_bottom_area
+
+
+def simulate(requested_water_temp, sampling_period, integration_constant):
+    # constants (put your real values here!)
+    heater_power = 3000
+    water_heat_const = 4184 #J/Kg
+    water_density = 997 # hg/m^3
+    tank_bottom_area = 0.2 # meter
+    tank_height = 1 # meter
+    water_flow = 0.035 # constant for input / output
+
+    temperature = [0]       # initial temperature
     e = []
-    Tw = 1
 
-    for n in range(10000) # in czas próbkowania
-        new_error, I_term = errorCalc(Tw, t[-1], tp, ti)
+    volume = calculateTankArea(tank_height, tank_bottom_area) #get tank volume
+    for n in range(1000):
+        new_error, I_term = errorCalc(requested_water_temp, temperature[-1], sampling_period, integration_constant)
         e.append(new_error)
-        x = (Pw * I_term * tp) / (cv * d * s * h) + (1 - (Qout / (s * h))) * t[-1]
-        t.append(x)
+
+        x = (heater_power * I_term * sampling_period) / (water_heat_const * water_density * volume) + (1 - (water_flow / volume)) * temperature[-1]
+        temperature.append(x)
+
+    return temperature
 
 
+# -------------------- Dash App --------------------
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    html.H1("Temperature Simulation (Tw, tp, ti)"),
+
+    html.Label("Tw (setpoint)"),
+    dcc.Slider(id='Tw', min=0, max=5, step=0.1, value=1),
+
+    html.Label("tp (sampling time)"),
+    dcc.Slider(id='tp', min=0.01, max=1, step=0.01, value=0.1),
+
+    html.Label("ti (integral time)"),
+    dcc.Slider(id='ti', min=0.1, max=10, step=0.1, value=1),
+
+    dcc.Graph(id='temp-graph')
+])
 
 
+@app.callback(
+    Output('temp-graph', 'figure'),
+    [Input('Tw', 'value'),
+     Input('tp', 'value'),
+     Input('ti', 'value')]
+)
+def update_graph(Tw, tp, ti):
+    t = simulate(Tw, tp, ti)
+
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(
+        y=t,
+        mode='lines',
+        name='Temperature'
+    ))
+    fig.update_layout(
+        title="Temperature Evolution",
+        xaxis_title="Iteration",
+        yaxis_title="Temperature"
+    )
+
+    return fig
 
 
-
+if __name__ == "__main__":
+    app.run(debug=True)
